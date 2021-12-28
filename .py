0bee64ff37cb905b7235500bd0e3d2c44cc0c23c
BINTANG-XD <94216075+bot-85@users.noreply.github.com>
2021-12-29 06:30:08 +0700

    Update .py

--- .py
+++ .py
@@ -256,7 +256,7 @@ def menu():
 	if sayangku=="1" or sayangku=="01":
 		publik()
 	elif sayangku=="2" or sayangku=="02":
-		info()
+		info_tools()
 	elif sayangku=="3" or sayangku=="03":
 		follow()
 	elif sayangku=="4" or sayangku=="04":
@@ -350,29 +350,40 @@ def result():
 		jalan("\n"+balmond+m+" Pilihan Anda Invalid")
 		time.sleep(0.5)
 		result()
+####INFO TOOLS####
+def info_tools():
+    os.system('clear')
+    logo()
+    print ' %s\x1b[1;93m[%s\x1b[1;93m#%s\x1b[1;93m]'%(N,O,N), 52 * '\x1b[1;97m-\x1b[0m';time.sleep(0.07)
+    print ' %s\x1b[1;93m[%s\x1b[1;96m>%s\x1b[1;93m] \x1b[1;92mWelcome to Tools       \x1b[1;93m'%(N,H,N);time.sleep(0.07)
+    print ' %s\x1b[1;93m[%s\x1b[1;96m>%s\x1b[1;93m] TEAM      \x1b[1;93m: XNX-CODE TEAM 2021.'%(N,H,N);time.sleep(0.07)
+    print ' %s\x1b[1;93m[%s\x1b[1;96m>%s\x1b[1;93m] Author    \x1b[1;93m: Sanz-Tzy \x1b[1;93mX Haris Ganz.'%(N,H,N);time.sleep(0.07)
+    print ' %s\x1b[1;93m[%s\x1b[1;96m>%s\x1b[1;93m] Github    \x1b[1;93m: https://github.com/Sanz-Tzy'%(N,H,N);time.sleep(0.07)
+    print ' %s\x1b[1;93m[%s\x1b[1;96m>%s\x1b[1;93m] Facebook  \x1b[1;93m: Bintang Tzy'%(N,H,N);time.sleep(0.07)
+    print ' %s\x1b[1;93m[%s\x1b[1;96m>%s\x1b[1;93m] Link FB   \x1b[1;93m: https://www.facebook.com/bintangt.zy.92'%(N,H,N);time.sleep(0.07)
+    print ' %s\x1b[1;93m[%s\x1b[1;96m>%s\x1b[1;93m] WhatsApp  \x1b[1;93m: +628727xxxx'%(N,H,N);time.sleep(0.07)
+    print ' %s\x1b[1;93m[%s\x1b[1;96m>%s\x1b[1;93m] Peringatan \x1b[1;93m: Jangan lupa Follow dan Like github saya!'%(N,H,N);time.sleep(0.07)
+    print ' %s\x1b[1;93m[%s\x1b[1;96m>%s\x1b[1;93m] Catatan  \x1b[1;93m: Kunjungi Facebook Saya Bintang Tzy'%(N,H,N);time.sleep(0.07)
+    print ' %s\x1b[1;93m[%s\x1b[1;93m#%s\x1b[1;93m]'%(N,O,N), 52 * '\x1b[1;97m-\x1b[0m';time.sleep(0.07)
+    print ' %s\x1b[1;93m[%s\x1b[1;96m>%s\x1b[1;93m] \x1b[1;92mTutorial buka akun sesi  \x1b[1;93m: '%(N,H,N);time.sleep(0.07)
+    print ' %s\x1b[1;93m[%s\x1b[1;96m>%s\x1b[1;93m] \x1b[1;93m: Buka akun terkena sesi'%(N,H,N);time.sleep(0.07)
+    print ' %s\x1b[1;93m[%s\x1b[1;96m>%s\x1b[1;93m] \x1b[1;93m: lalu liat jenis sesi tersebut jika sesi tersebut ia lah komputer lain'%(N,H,N);time.sleep(0.07)
+    print ' %s\x1b[1;93m[%s\x1b[1;96m>%s\x1b[1;93m] \x1b[1;93m: terus ketik lanjut 5-7 kli menggunakan SIM satu'%(N,H,N);time.sleep(0.07)
+    print ' %s\x1b[1;93m[%s\x1b[1;96m>%s\x1b[1;93m] \x1b[1;93m: lalu keluar dan ubah ke sim kedua'%(N,H,N);time.sleep(0.07)
+    print ' %s\x1b[1;93m[%s\x1b[1;96m>%s\x1b[1;93m] \x1b[1;93m: lalu mainkan mode pesawat 5-10 detik lalu masuk ke akun kesesi tersebut'%(N,H,N);time.sleep(0.07)
+    print ' %s\x1b[1;93m[%s\x1b[1;96m>%s\x1b[1;93m] \x1b[1;93m: lihat lah perubahan tersebut'%(N,H,N);time.sleep(0.07)
+    print ' %s\x1b[1;93m[%s\x1b[1;93m#%s\x1b[1;93m]'%(N,O,N), 52 * '\x1b[1;97m-\x1b[0m';time.sleep(0.07)
+    print ' %s\x1b[1;93m[%s\x1b[1;96m>%s\x1b[1;93m] \x1b[1;93m: \x1b[1;92mTerimakasih kepada'%(N,H,N);time.sleep(0.07)
+    print '\n %s\x1b[1;93m[%s\x1b[1;96m>%s\x1b[1;93m] \x1b[1;92m: Author Script'%(N,H,N);time.sleep(0.07)
+    print ' %s\x1b[1;93m[%s\x1b[1;96m>%s\x1b[1;93m] \x1b[1;93m: Sanz-Tzy'%(N,H,N);time.sleep(0.07)
+    print ' %s\x1b[1;93m[%s\x1b[1;96m>%s\x1b[1;93m] \x1b[1;93m: Haris Ganz'%(N,H,N);time.sleep(0.07)
+    print '\n %s\x1b[1;93m[%s\x1b[1;96m>%s\x1b[1;93m] \x1b[1;92m: Team Script'%(N,H,N);time.sleep(0.07)
+    print ' %s\x1b[1;93m[%s\x1b[1;96m>%s\x1b[1;93m] \x1b[1;93m: YumasaaTzy'%(N,H,N);time.sleep(0.07)
+    print ' %s\x1b[1;93m[%s\x1b[1;96m>%s\x1b[1;93m] \x1b[1;93m: Mrjeeck X nano'%(N,H,N);time.sleep(0.07)
+    print ' %s\x1b[1;93m[%s\x1b[1;96m>%s\x1b[1;93m] \x1b[1;93m: aang Ardiansyah XD'%(N,H,N);time.sleep(0.07)
+    print ' %s\x1b[1;93m[%s\x1b[1;96m>%s\x1b[1;93m] \x1b[1;93m: Yumee Tzy'%(N,H,N);time.sleep(0.07)
+    raw_input('\n [ %s\x1b[1;92mKEMBALI%s \x1b[1;93m] '%(O,N));menu()
 
-# Jancok
-def info()
-
-    jalan('%s╠══[%s•%s] %sNama : %s'%(O,P,O,P,nm))
-    jalan('%s╠══[%s•%s] %sNama Depan : %s'%(O,P,O,P,nd))
-    jalan('%s╠══[%s•%s] %sNama Tengah : %s'%(O,P,O,P,nt))
-    jalan('%s╠══[%s•%s] %sNama Belakang : %s'%(O,P,O,P,nb))
-    jalan('%s╠══[%s•%s] %sTTL : %s'%(O,P,O,P,ut))
-    jalan('%s╠══[%s•%s] %sGender : %s'%(O,P,O,P,gd))
-    jalan('%s╠══[%s•%s] %sEmail : %s'%(O,P,O,P,em))
-    jalan('%s╠══[%s•%s] %sLink : %s'%(O,P,O,P,lk))
-    jalan('%s╠══[%s•%s] %sUsername : %s'%(O,P,O,P,us))
-    jalan('%s╠══[%s•%s] %sAgama : %s'%(O,P,O,P,rg))
-    jalan('%s╠══[%s•%s] %sStatus Hubungan : %s'%(O,P,O,P,rl))
-    jalan('%s╠══[%s•%s] %sHubungan Dengan : %s'%(O,P,O,P,rls))
-    jalan('%s╠══[%s•%s] %sTempat Tinggal : %s'%(O,P,O,P,lc))
-    jalan('%s╠══[%s•%s] %sTempat Asal : %s'%(O,P,O,P,ht))
-    jalan('%s╠══[%s•%s] %sTentang : %s'%(O,P,O,P,ab))
-    jalan('%s╠══[%s•%s] %sLocale : %s'%(O,P,O,P,lo))
-    print('%s║'%(O))
-    input('%s╚══[ %sReturn %s]%s'%(O,P,O,P))
-    menu()
 # USER AGENT
 
 def user_agent():
